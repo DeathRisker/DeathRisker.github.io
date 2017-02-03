967e2d3167e850dd02d8006302137f2f03abe28d
Hantong Li <h324li@edu.uwaterloo.ca>
2017-02-02 19:41:03 -0500

    fix menu bug

--- js/helper.js
+++ js/helper.js
@@ -1,10 +1,47 @@
-$('input:checkbox').prop('checked', false);
+// Reset page onload event
 
-function toggleSidebar(checkbox){
-    if(checkbox.checked){
-        $("#sidebar").show();
+// based on Todd Motto functions
+// http://toddmotto.com/labs/reusable-js/
+
+// hasClass
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// // addClass
+// function addClass(elem, className) {
+//     if (!hasClass(elem, className)) {
+//     	elem.className += ' ' + className;
+//     }
+// }
+// // removeClass
+// function removeClass(elem, className) {
+// 	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+// 	if (hasClass(elem, className)) {
+//         while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+//             newClass = newClass.replace(' ' + className + ' ', ' ');
+//         }
+//         elem.className = newClass.replace(/^\s+|\s+$/g, '');
+//     }
+// }
+// toggleClass
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
     }
-    else {
-        $("#sidebar").hide();
-    }
+}
+
+function toggleMenu(menu){
+    toggleClass(menu, 'on');
+    var mainPage = document.getElementById("main-page");
+    toggleClass(mainPage, 'on');
+    var sidebar = document.getElementById("sidebar");
+    toggleClass(sidebar, 'on');
+
+    return false;
 }
